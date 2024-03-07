c2f329c3b9b536947301ff3c061b9cdbb1df7cba
Lexcai <axel.boudeau@ynov.com>
2024-03-07 16:06:32 +0100

    Ajout des fichiers discover.js et movies.js dans le dossier pages/api et modification du fichier config.services.js

--- services/config.services.js
+++ services/config.services.js
@@ -2,12 +2,12 @@ export const ConfigService = {
   themoviedb: {
     urls: {
       discover: "https://api.themoviedb.org/3/discover/movie",
+      movie: "https://api.themoviedb.org/3/movie",
     },
     keys: {
       API_TOKEN:
         "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMWU4MDRiNTc4ZTk0N2I2NDM3MDk3NTNjNGZiN2I5ZCIsInN1YiI6IjY1ZTliODAzNWFiYTMyMDE4NjcwZjEzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LXpL4w8P7XwhPjefLL7fkRbzCGm3VIt_TXbtuDcLfbk",
       API_KEY: "11e804b578e947b643709753c4fb7b9d",
     },
-    language: "fr-FR",
   },
 };
